4ff041a14cc246c2e78f01829cf3134fa1e03204
jaapyang <15873115257@qq.com>
2017-11-29 23:00:22 +0800

    practice for requests

--- learning_for_spider/sprint01_learning_for_Urllib/practice_for_requests.py
+++ learning_for_spider/sprint01_learning_for_Urllib/practice_for_requests.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 # -*-coding:utf-8-*-
+import os
 import requests
 import json
+
 
 # response = requests.get("http://www.baidu.com")
 # print(type(response))
@@ -23,5 +25,42 @@ import json
 # r = requests.put('http://httpbin.org/put')
 # print(r.text)
 
-r = requests.head('http://httpbin.org/get')
-print(r.json())
+def practice_post():
+    data = {
+        "name": "paul",
+        "age": 34
+    }
+    response = requests.post("http://httbin.org/post", data=data)
+    print(response.text)
+
+
+def practice_upload_file():
+    files = {"files": open("Static\\test.txt", "rb")}
+    response = requests.post("http://httpbin.org/post", files=files)
+    print(response.text)
+
+
+def take_cookie():
+    s = requests.Session()
+    s.get("http://httpbin.org/cookies/set/number/234567")
+    response = s.get("http://httpbin.org/cookies")
+    print(response.text)
+
+
+def set_proxies():
+    """
+    这里有问题
+    """
+    proxies = {
+        "http": "http://112.114.97.137:8118",
+        "https": "https://171.39.9.40:8123",
+    }
+    response = requests.get("https://www.baidu.com", proxies=proxies)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    # practice_post()
+    # practice_upload_file()
+    # take_cookie()
+    set_proxies()
