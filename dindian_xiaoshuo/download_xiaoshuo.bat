8456b5f5b26cd4778d723f9e74e19b3ac70e1e2a
jaapyang <15873115257@qq.com>
2017-12-03 01:54:27 +0800

    - 综合使用代理 - 通过爬虫获取代理 - 通过访问指定网站验证代理

---
+++ dindian_xiaoshuo/download_xiaoshuo.bat
@@ -0,0 +1,1 @@
+@downloadxiaoshuo.py
--- dindian_xiaoshuo/downloadxiaoshuo.py
+++ dindian_xiaoshuo/downloadxiaoshuo.py
@@ -1,13 +1,30 @@
 #! /usr/bin/env python3
 # -*-coding:utf-8-*-
+import logging
 import os
+import random
 import re
-
 import requests
+import time
 from bs4 import BeautifulSoup
 
-LIST_URL = 'http://www.23us.so/files/article/html/14/14741/index.html'
+LIST_URL = 'http://www.23us.so/files/article/html/0/240/index.html'
 SAVE_DIR = 'D:\\download_books'
+SLEEP_TIME = 3
+http_proxies_list = [
+    # {"http": "http://115.28.148.137:8118", "https": "https://112.114.99.133:8118"},
+    {"http": "http://106.75.56.87:80", "https": "https://123.53.137.32:48420"},
+    # {"http": "http://221.233.85.18:3128", "https": "https://118.72.124.97:80"},
+    # {"http": "http://xxxx:xxx", "https": "https://xxxx:8118"},
+    # {"http": "http://xxxx:xxx", "https": "https://xxxx:8118"},
+    # {"http": "http://xxxx:xxx", "https": "https://xxxx:8118"},
+    # {"http": "http://xxxx:xxx", "https": "https://xxxx:8118"},
+]
+
+headers_list = [
+    {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"},
+]
 
 
 class Charector():
@@ -16,10 +33,21 @@ class Charector():
         self.Title = title
 
 
+def random_get_the_first_from_list(data_list):
+    random.shuffle(data_list)
+    data = data_list[0]
+    # print(data)
+    return data
+
+
 def get_page_html(url):
     """获取指定URL页面的HTML"""
-    response = requests.get(url)
-    return response.content.decode('utf-8')
+    time.sleep(SLEEP_TIME)
+    # proxies = random_get_the_first_from_list(http_proxies_list)
+    headers = random_get_the_first_from_list(headers_list)
+    with requests.get(url, headers=headers) as response:
+        # response = requests.get(url, proxies=proxies, headers=headers)
+        return response.content.decode('utf-8')
 
 
 def parse_list(menu_html):
@@ -48,19 +76,54 @@ def read_txt_content_by_line(path):
 
 
 if __name__ == '__main__':
-    lines = read_txt_content_by_line(os.path.join(SAVE_DIR, '超维术士.txt'))
-    for line in lines:
-        print(line)
-    # menu_html = get_page_html(LIST_URL)
-    # charact_list = parse_list(menu_html)
-    # with open(os.path.join(SAVE_DIR, '14741.txt'), 'w', encoding='utf-8') as f:
-    #     for item in charact_list:
-    #         print(item.Href + '/' + item.Title)
-    #         page_html = get_page_html(item.Href)
-    #         page_content = parse_detail_page_content(page_html)
-    #         f.write(item.Title)
-    #         f.write(page_content)
+    """测试动态代理"""
+    # content = get_page_html("http://httpbin.org/headers")
+    # print(content)
 
-        # html = get_page_html('http://www.23us.so/files/article/html/14/14741/9528946.html')
-        # content = parse_detail_page_content(html)
-        # save_to_file('01 test',content)
+    """输出已经下载的小说内容"""
+    # lines = read_txt_content_by_line(os.path.join(SAVE_DIR, '巫师之旅.txt'))
+    # for line in lines:
+    #     print(line)
+
+    """下载小说"""
+
+    LIST_URL = input("请输入列表页网址:")
+    file_name = input("请输入保存的文件名:")
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(levelname)s \t %(asctime)s ---------- \n\t%(message)s\n',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=os.path.join(SAVE_DIR, 'log', file_name + '.log'),
+                        filemode='w')
+
+    menu_html = get_page_html(LIST_URL)
+    charact_list = parse_list(menu_html)
+    with open(os.path.join(SAVE_DIR, file_name + '.txt'), 'w', encoding='utf-8') as f:
+        index = 0
+        for item in charact_list:
+            index = index + 1
+            if index > 30:
+                SLEEP_TIME = 60
+                index = 0
+            else:
+                SLEEP_TIME = random.randint(1, 10)
+
+            running_message = item.Href + '/' + item.Title + '\tindex:' + str(index) + '\tSleep:' + str(SLEEP_TIME)
+            print(running_message)
+            logging.info(running_message)
+            try:
+                page_html = get_page_html(item.Href)
+                page_content = parse_detail_page_content(page_html)
+                f.write("\n")
+                f.write(item.Title)
+                f.write("\n")
+                f.write(page_content)
+            except BaseException as e:
+                logging.error(e)
+                print(e)
+    input("下载结束，请手动关闭.....")
+
+    """测试分章节下载小说"""
+    # html = get_page_html('http://www.23us.so/files/article/html/14/14741/9528946.html')
+    # content = parse_detail_page_content(html)
+    # save_to_file('01 test',content)
