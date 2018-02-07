b7abde75300981b203b895ba7e3af62a54bd3424
jaapyang <15873115257@qq.com>
2018-02-07 23:28:34 +0800

    笔趣阁解析

---
+++ com.miaow/ToolPlat/Handlers/BiQuGeXiaoShuoHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using NSoup;
+using NSoup.Nodes;
+
+namespace ToolPlat.Handlers
+{
+    public class BiQuGeXiaoShuoHandler : HandlerBase
+    {
+        public BiQuGeXiaoShuoHandler(IWebBowserForm parentBowserForm) : base(parentBowserForm)
+        {
+        }
+
+        public void Parse_Menu_Url(string url)
+        {
+            var domainUrl = "http://www.biquge.com.tw";
+            var connect = NSoupClient.Connect(url);
+            var document = connect.Get();
+            var linkArray = document?.GetAllElements().Where(x => x.TagName() == "a").ToList();
+
+            var pattern = @"/\d+_\d+/\d+.html";
+
+            var arrayList = new ArrayList();
+
+            foreach (Element element in linkArray)
+            {
+                var href = element.Attr("href");
+                if (!element.HasText || !Regex.IsMatch(href, pattern)) continue;
+
+                var text = element.Text();
+
+                arrayList.Add(new
+                {
+                    Url = $"{domainUrl}{href}",
+                    Title = text
+                });
+            }
+
+            var argsStr = JsonConvert.SerializeObject(arrayList);
+
+            InvokeScriptFunction("loadMenu", argsStr);
+        }
+    }
+}
